b0c8beae55568e0a5a27b6a5679f7210f63a6a40
pspanoudakis <pavlos.spanoudakis@gmail.com>
2020-10-15 23:26:48 +0300

    Implementing findPathToClosestDot

--- searchAgents.py
+++ searchAgents.py
@@ -550,7 +550,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.uniformCostSearch(problem)
 
 
 class AnyFoodSearchProblem(PositionSearchProblem):
@@ -586,8 +586,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         """
         x,y = state
 
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 
 def mazeDistance(point1, point2, gameState):
