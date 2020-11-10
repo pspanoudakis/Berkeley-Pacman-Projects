4150932f1c641ec528d00239631d00cb8077b56b
pspanoudakis <pavlos.spanoudakis@gmail.com>
2020-11-10 16:26:45 +0200

    Improving ReflexAgent

--- multiAgents.py
+++ multiAgents.py
@@ -77,11 +77,15 @@ class ReflexAgent(Agent):
 
         ghostPositions = successorGameState.getGhostPositions()
         for p in ghostPositions:
+            # If a ghost is at the new position or next to it
             if p == newPos or util.manhattanDistance(p, newPos) == 1:
+                # Don't go there
                 return(float('-inf'))
+            # If there is food in the new position
             elif currentFood[newPos[0]][newPos[1]]:
-                if util.manhattanDistance(p, newPos) >= 2:
-                    return float('inf')
+                # There is no ghost there or next to it,
+                # so go there and eat the dot
+                return float('inf')
         
         # Pacman is not in immediate danger in the new position, but there is no food there
         # Now estimating the nearest food dot:
@@ -91,7 +95,9 @@ class ReflexAgent(Agent):
             dist = util.manhattanDistance(f, newPos)
             if dist < minDist:
                 minDist = dist
-        #return successorGameState.getScore()
+        
+        # We return -minDist, because the higher value is better,
+        # and a state near a food dot is more preferable.
         return -minDist
 
 def scoreEvaluationFunction(currentGameState):
@@ -164,7 +170,6 @@ class MinimaxAgent(MultiAgentSearchAgent):
 
     def minValue(self, gameState, currDepth, currAgent):
         if gameState.isWin() or gameState.isLose() or currDepth == self.depth:
-            #return gameState.getScore()
             return self.evaluationFunction(gameState)
         actions = gameState.getLegalActions(currAgent)
         successors = []
@@ -178,7 +183,6 @@ class MinimaxAgent(MultiAgentSearchAgent):
     
     def maxValue(self, gameState, currDepth):
         if gameState.isWin() or gameState.isLose() or currDepth == self.depth:
-            #return gameState.getScore()
             return self.evaluationFunction(gameState)
         actions = gameState.getLegalActions(0)
         successors = []
@@ -252,6 +256,14 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         """
         "*** YOUR CODE HERE ***"
         util.raiseNotDefined()
+
+    def maxExpect(self, gameState, currDepth):
+        pass
+
+    def minExpect(self, gameState, currDepth, currAgent):
+        pass
+    def chanceExpect(self, gameState):
+        pass
 
 def betterEvaluationFunction(currentGameState):
     """
